94bcfae409c98240f494795b003f4d62bdb5ff20
Banks Nussman <banks@nussman.us>
2026-09-02 21:29:48 -0400

    chore: clean up playground file

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import { subscribers } from "./v1";
-
-const me = await subscribers({ path: {app_user_id: "banks" }})
-
-const t = me.data?.subscriber?.non_subscriptions['my-thing'][0];
-
-const p = t;
